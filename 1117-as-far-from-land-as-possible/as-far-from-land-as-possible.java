c6111c77f2faa1df5c43f112bfec2c00c03007ca
Channaveersh s malaguddappanavar <137072238+channaveereshsm@users.noreply.github.com>
2024-05-17 13:28:13 +0530

    Time: 2042 ms (5.04%) | Memory: 45.6 MB (52.33%) - LeetSync

---
+++ 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int maxDistance(int[][] grid) {
+        List<Integer> lx=new ArrayList<>();
+        List<Integer> ly=new ArrayList<>();
+        for(int i=0;i<grid.length;i++)
+        {
+            for(int j=0;j<grid[i].length;j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    lx.add(i);
+                    ly.add(j);
+                }
+            }
+        }
+        int max=-1;
+        for(int i=0;i<grid.length;i++)
+        {
+            for(int j=0;j<grid.length;j++)
+            {
+                if(grid[i][j]==0)
+                {
+                    int min=1000000;
+                    for(int k=0;k<lx.size();k++)
+                    {
+                      min=Math.min(min,Math.abs(lx.get(k)-i)+Math.abs(ly.get(k)-j));  
+                    }
+                    if(min!=1000000)
+                    {
+                        max=Math.max(max,min);
+                    }
+                }
+            }
+        }
+        return max;
+    }
+}

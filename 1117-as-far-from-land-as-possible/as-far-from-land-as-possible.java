9b8f5d7e4c26704c7c55594af1c19b7d791e01b7
Channaveersh s malaguddappanavar <137072238+channaveereshsm@users.noreply.github.com>
2024-05-17 14:08:24 +0530

    Time: 16 ms (39.15%) | Memory: 45.6 MB (52.33%) - LeetSync

--- 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.java
+++ 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.java
@@ -1,39 +1,55 @@
 class Solution {
+    int ans=0;
     public int maxDistance(int[][] grid) {
-        List<Integer> lx=new ArrayList<>();
-        List<Integer> ly=new ArrayList<>();
+        Queue<Integer> qx=new LinkedList<>();
+        Queue<Integer> qy=new LinkedList<>();
+        int dp[][]=new int[grid.length][grid[0].length];
+        
         for(int i=0;i<grid.length;i++)
         {
-            for(int j=0;j<grid[i].length;j++)
+            for(int j=0;j<grid[0].length;j++)
             {
-                if(grid[i][j]==1)
-                {
-                    lx.add(i);
-                    ly.add(j);
-                }
+              if(grid[i][j]==1)
+              {
+                  grid[i][j]=0;
+                  dp[i][j]=1;
+                  qx.add(i);
+                  qy.add(j);
+              }
             }
         }
-        int max=-1;
-        for(int i=0;i<grid.length;i++)
+        if(qx.size()==grid.length*grid[0].length)
+            return -1;
+        if(qx.size()==0)
+            return -1;
+        int d[][]={{-1,0},{0,-1},{1,0},{0,1}};
+         
+        while(qx.size()!=0)
         {
-            for(int j=0;j<grid.length;j++)
+            ans=ans+1;
+            int size=qx.size();
+            for(int i=0;i<size;i++)
             {
-                if(grid[i][j]==0)
+                int x=qx.poll();
+                int y=qy.poll();
+                for(int j=0;j<4;j++)
                 {
-                    int min=1000000;
-                    for(int k=0;k<lx.size();k++)
-                    {
-                      min=Math.min(min,Math.abs(lx.get(k)-i)+Math.abs(ly.get(k)-j)); 
-                        if(min==1)
-                            break;
-                    }
-                    if(min!=1000000)
-                    {
-                        max=Math.max(max,min);
-                    }
+                    int x1=x+d[j][0];
+                    int y1=y+d[j][1];
+                    if(x1<0||y1<0||x1>dp.length-1||y1>dp[0].length-1||dp[x1][y1]==1)
+                        continue;
+                    dp[x1][y1]=1;
+                    grid[x1][y1]=ans;
+                    
+                    qx.add(x1);
+                    qy.add(y1);
                 }
             }
+            
         }
-        return max;
+        
+            
+        
+        return ans-1;
     }
 }

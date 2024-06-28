5468452f3741892aa9b2460d41ddffdf722f631c
Channaveersh s malaguddappanavar <137072238+channaveereshsm@users.noreply.github.com>
2024-06-28 13:22:03 +0530

    Time: 1 ms (81.64%) | Memory: 41.2 MB (51.07%) - LeetSync

--- 290-word-pattern/word-pattern.java
+++ 290-word-pattern/word-pattern.java
@@ -1,28 +1,27 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
         String a[]=s.split(" ");
-        HashMap<Character,String> m=new HashMap<>();
-         HashMap<String,Character> m1=new HashMap<>();
+         HashMap<String,Character> m=new HashMap<>();
 
         if(pattern.length()!=a.length)
         return false;
-           for(int i=0;i<pattern.length();i++)
+        int b[]=new int[26];
+           for(int i=0;i<a.length;i++)
            {
-               if(!m.containsKey(pattern.charAt(i)))
-               {
-                 if(m1.containsKey(a[i]))
-                 return false;
-                 else
-                 {
-                    m.put(pattern.charAt(i),a[i]);
-                     m1.put(a[i],pattern.charAt(i));
-                 }
-               }
-               else
-               {
-                 String s1=m.get(pattern.charAt(i));
-                 if(!s1.equals(a[i]))
-                 return false;}
+            if(!m.containsKey(a[i]))
+            {
+                if(b[pattern.charAt(i)-'a']==1)
+                return false;
+                  b[pattern.charAt(i)-'a']=1;
+                m.put(a[i],pattern.charAt(i));
+            }
+            else
+            {
+                char c=m.get(a[i]);
+                if(c!=pattern.charAt(i))
+                return false;
+            }
+
            }
     
         return true;

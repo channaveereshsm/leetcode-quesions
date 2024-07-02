e12a0efd33bd556bbc6857354048bf0513523621
Channaveersh s malaguddappanavar <137072238+channaveereshsm@users.noreply.github.com>
2024-07-02 21:55:17 +0530

    Time: 12 ms (93.73%) | Memory: 43.8 MB (95.48%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,6 +1,6 @@
 class Solution {
     public int coinChange(int[] c, int a) {
-        Arrays.sort(c);
+   
         int dp[]=new int[a+1];
         Arrays.fill(dp,100000);
         dp[0]=0;
@@ -10,12 +10,12 @@ class Solution {
             {
                 for(int j=c[i];j<=a;j++)
                 {
-                    if(dp[j-c[i]]!=0 || j-c[i]==0)
-                    {
+                    
                         dp[j]=Math.min(dp[j],dp[j-c[i]]+1);
-                    }
+                    
                 }
             }
+           
         }
         if(dp[a]>=100000)
         return -1;

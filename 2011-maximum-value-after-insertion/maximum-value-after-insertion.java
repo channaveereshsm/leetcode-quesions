f332a3a22c33a3a4954b572d86cc8e0d1cde9282
Channaveersh s malaguddappanavar <137072238+channaveereshsm@users.noreply.github.com>
2024-07-04 02:01:54 +0530

    Time: 12 ms (75.25%) | Memory: 45.9 MB (25.74%) - LeetSync

--- 2011-maximum-value-after-insertion/maximum-value-after-insertion.java
+++ 2011-maximum-value-after-insertion/maximum-value-after-insertion.java
@@ -1,46 +1,28 @@
 class Solution {
     public String maxValue(String n, int x) {
-        if(n.charAt(0)=='-')
-        {
-            int p=-1,max=0;
-            for(int i=1;i<n.length();i++)
-            {
-                int d=n.charAt(i)-'0';
-                if(d>x)
-                {
-                  
-                  p=i;
-                  break;
-                
+        if (n.charAt(0) == '-') {
+            int p = n.length();
+            for (int i = 1; i < n.length(); i++) {
+                int d = n.charAt(i) - '0';
+                if (d > x) {
+
+                    p = i;
+                    break;
                 }
             }
-            if(p!=-1)
-             n=n.substring(0,p)+x+""+n.substring(p);
-             else
-              n=n+""+x;
-        }
-        else
-        {
-            int l=n.length(),f=0,max=9,p=-1;
-            for(int i=0;i<l;i++)
-            {
-                int d=n.charAt(i)-'0';
-                if(x>d )
-                {
-                    if(max>d && p==-1)
-                    {
-                        p=i;
-                        max=d;
-                    }
-                    //  n=n.substring(0,i)+x+""+n.substring(i);
-                    //  f=1;
-                    //  break;
+            n = n.substring(0, p) + x + "" + n.substring(p);
+
+        } else {
+            int p = n.length();
+            for (int i = 0; i < n.length(); i++) {
+                int d = n.charAt(i) - '0';
+                if (x > d) {
+                    p = i;
+                    break;
                 }
             }
-            if(p!=-1)
-             n=n.substring(0,p)+x+""+n.substring(p);
-             else
-              n=n+""+x;
+
+            n = n.substring(0, p) + x + "" + n.substring(p);
         }
         return n;
     }
